64291b1368335375f756a0a01322389fb6a5109c
AmenallahMeftehh <meftah.amenallah@gmail.com>
2016-04-16 10:31:32 +0100

    ajouter le routage entre les pages et finir le menu

---
+++ app.js
@@ -0,0 +1,35 @@
+angular.module('app',['ngRoute']).config(['$routeProvider',function($routeProvider){
+$routeProvider
+.when('/login',{
+  templateUrl:'pages/login.html',
+
+})
+.when('/home',{
+  templateUrl:'pages/home.html',
+
+})
+.when('/panier',{
+  templateUrl:'pages/panier.html',
+
+})
+.when('/football',{
+  templateUrl:'pages/football.html',
+
+})
+.when('/golf',{
+  templateUrl:'pages/golf.html',
+
+})
+.when('/cyclisme',{
+  templateUrl:'pages/cyclisme.html',
+
+})
+.when('/tennis',{
+  templateUrl:'pages/tennis.html',
+
+})
+.otherwise({
+    redirectTo: '/home'
+          })
+
+}]);
