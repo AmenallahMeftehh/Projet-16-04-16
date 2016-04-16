e37d1f400a4b4ac2d3ddc16ae3e043e1b367862a
AmenallahMeftehh <meftah.amenallah@gmail.com>
2016-04-16 15:23:39 +0100

    ajout de modal

--- Controllers/homeController.js
+++ Controllers/homeController.js
@@ -1,7 +1,7 @@
 angular.module('app').controller('homeController',['$scope',function($scope){
      $(document).ready(function() {
                  $('.carousel').carousel({
-                     interval: 5000
+                     interval: 1000
                  })
              });
 
--- Controllers/tennisController.js
+++ Controllers/tennisController.js
@@ -1,5 +1,5 @@
 angular.module('app').controller('tennisController',['$scope', '$http',function($scope,$http){
   $http.get('data/dataTennis.json').success(function(data) {
-     $scope.tennis= data;
+     $scope.tennis = data;
 })
 }]);
