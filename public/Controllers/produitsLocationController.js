7072c5b84561569272072e43c1811af521c9583d
amenallahmeftehh <meftah.amenallah@gmail.com>
2016-05-18 00:54:04 +0200

    design complet

--- public/Controllers/produitsLocationController.js
+++ public/Controllers/produitsLocationController.js
@@ -2,11 +2,7 @@ angular.module('app').controller('produitLocationController', ['$scope', '$locat
         , function ($scope, $location, $routeParams,AuthService, $rootScope,$http) {
 
           $scope.produit={};
-          // var id =$routeParams.itemId;
-          // // console.log(id);
-          // $http.get('/produits/'+id).success(function(data){
-          //   $scope.produit = data;});
-
+        
           $http.get('/produits/location').success(function(data) {
            $scope.locations= data;
           });
@@ -18,32 +14,6 @@ angular.module('app').controller('produitLocationController', ['$scope', '$locat
           $scope.prix=500;
           $scope.produits = [] ;
 
-          // $scope.reservations=[];
-          // $scope.reservation={};
-          //
-          // var getReservations = function () {
-          //   $http.get('/produits/'+id).success(function(data) {
-          //    $scope.reservations= data.reservation;
-          //       console.log('aa');
-          //         console.log($scope.reservations);
-          //         console.log('bb');
-          //         for (var i = 0; i <   $scope.reservations.length; i++) {
-          //            $http.get('/produits/'+$scope.reservations[i]).success(function(data){
-          //              $scope.reservation=data;
-          //               console.log(data);
-          //               console.log('ccc');
-          //                $scope.reservations.push($scope.reservation);
-          //
-          //                console.log('aaaaa');
-          //
-          //          });
-          //
-          //              }
-          //         console.log('i received the data i requested');
-          //     });
-          //   };
-          //
-          // // appel au fonction getReservation()
-          // getReservations()
+
 
             }]);
